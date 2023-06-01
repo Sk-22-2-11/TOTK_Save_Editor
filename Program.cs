03abda98512d9e43cfb63146e33a6715e486b9ed
Derek <sandaeknight@gmail.com>
2023-06-01 13:00:59 -0400

    Demo_place holder

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+
+namespace _totk_save_editor
+{
+    class SaveEditor
+    {
+        static void Main(string[] args)
+        {
+            byte number_1 = 0;
+            Console.WriteLine("{0}; {1}; {2};",number_1,byte.MaxValue,int.MaxValue);
+        }
+    }
+}
